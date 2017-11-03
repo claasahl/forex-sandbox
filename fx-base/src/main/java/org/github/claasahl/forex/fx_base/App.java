da06eadc37922ce293d04ab817bb1806cfc5400c
Claas Ahlrichs <claasahl@gmail.com>
2017-11-03 16:37:22 +0100

    initial setup of maven project

---
+++ fx-base/src/main/java/org/github/claasahl/forex/fx_base/App.java
@@ -0,0 +1,13 @@
+package org.github.claasahl.forex.fx_base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
