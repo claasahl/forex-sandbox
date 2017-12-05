bb0046ca3474833ab6a61c0798191ab8eae4f821
Claas Ahlrichs <claasahl@gmail.com>
2017-12-05 07:00:58 +0100

    drafted helper function for stream of "variable" date-time objects

--- fx-base/src/main/java/org/github/claasahl/forex/data/DateTimeHelper.java
+++ fx-base/src/main/java/org/github/claasahl/forex/data/DateTimeHelper.java
@@ -1,9 +1,11 @@
 package org.github.claasahl.forex.data;
 
 import java.time.*;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import io.reactivex.*;
 import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.*;
 
 public class DateTimeHelper {
 	public static Observable<OffsetDateTime> live(final Duration duration, final OffsetDateTime from) {
@@ -16,8 +18,21 @@ public class DateTimeHelper {
 				.map(multiplicand -> from.plus(duration.multipliedBy(multiplicand)));
 	}
 
-	public static Observable<OffsetDateTime> live(final OffsetDateTime from) {
-		return Observable.empty();
+	public static Observable<OffsetDateTime> live(final Duration minDuration, final Duration maxDuration,
+			final Random random, final OffsetDateTime from) {
+		return live(minDuration, maxDuration, random, from, Schedulers.computation());
+	}
+
+	public static Observable<OffsetDateTime> live(final Duration minDuration, final Duration maxDuration,
+			final Random random, final OffsetDateTime from, Scheduler scheduler) {
+		final int variation = (int) (maxDuration.toMillis() - minDuration.toMillis());
+		Subject<Duration> delay = BehaviorSubject.createDefault(Duration.ZERO);
+		return delay.concatMap(duration -> wait(duration, scheduler).doOnComplete(() -> {
+			Duration millis = Duration.ofMillis(random.nextInt(variation));
+			delay.onNext(minDuration.plus(millis));
+		})).scan(Duration.ZERO, Duration::plus)
+				.map(duration -> from.plus(duration))
+				.distinctUntilChanged();
 	}
 
 	public static Observable<OffsetDateTime> historic(final Duration duration, final OffsetDateTime from) {
@@ -30,5 +45,24 @@ public class DateTimeHelper {
 	public static Observable<OffsetDateTime> historic(final Duration duration, final OffsetDateTime from,
 			final OffsetDateTime to) {
 		return historic(duration, from).takeWhile(d -> !d.isAfter(to));
+	}
+
+	public static Observable<OffsetDateTime> historic(final Duration minDuration, final Duration maxDuration,
+			final Random random, final OffsetDateTime from) {
+		final int variation = (int) (maxDuration.toMillis() - minDuration.toMillis());
+		return Observable.<OffsetDateTime, OffsetDateTime>generate(() -> from, (dateTime, emitter) -> {
+			emitter.onNext(dateTime);
+			Duration millis = Duration.ofMillis(random.nextInt(variation));
+			return dateTime.plus(minDuration.plus(millis));
+		});
+	}
+
+	public static Observable<OffsetDateTime> historic(final Duration minDuration, final Duration maxDuration,
+			final Random random, final OffsetDateTime from, final OffsetDateTime to) {
+		return historic(minDuration, maxDuration, random, from).takeWhile(d -> !d.isAfter(to));
+	}
+
+	private static Observable<Duration> wait(Duration duration, Scheduler scheduler) {
+		return Observable.timer(duration.toMillis(), TimeUnit.MILLISECONDS, scheduler).map(ignored -> duration);
 	}
 }
--- fx-base/src/test/java/org/github/claasahl/forex/data/DateTimeHelperTest.java
+++ fx-base/src/test/java/org/github/claasahl/forex/data/DateTimeHelperTest.java
@@ -1,13 +1,13 @@
 package org.github.claasahl.forex.data;
 
 import java.time.*;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 import io.reactivex.observers.TestObserver;
 import io.reactivex.schedulers.TestScheduler;
 
 public class DateTimeHelperTest {
-
 	@Test
 	public void shouldEmitLiveDateTimes() {
 		TestScheduler scheduler = new TestScheduler();
@@ -37,11 +37,43 @@ public class DateTimeHelperTest {
 				OffsetDateTime.of(2017, 11, 1, 15, 26, 0, 0, ZoneOffset.UTC))
 				.assertComplete();
 	}
+	
+	@Test
+	public void shouldEmitVariableLiveDateTimes() {
+		TestScheduler scheduler = new TestScheduler();
+		Duration minDuration = Duration.ofSeconds(10);
+		Duration maxDuration = Duration.ofMinutes(1);
+		Random random = new Random(1);
+		OffsetDateTime from = OffsetDateTime.of(2017, 11, 1, 15, 23, 0, 0, ZoneOffset.UTC);
+
+		TestObserver<OffsetDateTime> test = DateTimeHelper
+				.live(minDuration, maxDuration, random, from, scheduler)
+				.take(4)
+				.test();
+		scheduler.advanceTimeTo(58984, TimeUnit.MILLISECONDS);
+		test.assertValueCount(1);
+		scheduler.advanceTimeTo(58985, TimeUnit.MILLISECONDS);
+		test.assertValueCount(2);
+
+		scheduler.advanceTimeTo(83572, TimeUnit.MILLISECONDS);
+		test.assertValueCount(2);
+		scheduler.advanceTimeTo(83573, TimeUnit.MILLISECONDS);
+		test.assertValueCount(3);
+
+		scheduler.advanceTimeTo(135419, TimeUnit.MILLISECONDS);
+		test.assertValueCount(3);
+		scheduler.advanceTimeTo(135420, TimeUnit.MILLISECONDS);
+		test.assertValues(OffsetDateTime.of(2017, 11, 1, 15, 23, 0, 0, ZoneOffset.UTC),
+				OffsetDateTime.of(2017, 11, 1, 15, 23, 58, 985000000, ZoneOffset.UTC),
+				OffsetDateTime.of(2017, 11, 1, 15, 24, 23, 573000000, ZoneOffset.UTC),
+				OffsetDateTime.of(2017, 11, 1, 15, 25, 15, 420000000, ZoneOffset.UTC))
+				.assertComplete();
+	}
 
 	@Test
 	public void shouldEmitUnlimitedHistoricDateTimes() {
-		OffsetDateTime from = OffsetDateTime.of(2017, 11, 1, 15, 23, 0, 0, ZoneOffset.UTC);
 		Duration duration = Duration.ofMinutes(1);
+		OffsetDateTime from = OffsetDateTime.of(2017, 11, 1, 15, 23, 0, 0, ZoneOffset.UTC);
 
 		DateTimeHelper.historic(duration, from)
 				.take(3)
@@ -54,9 +86,9 @@ public class DateTimeHelperTest {
 	
 	@Test
 	public void shouldEmitLimitedHistoricDateTimes() {
+		Duration duration = Duration.ofMinutes(1);
 		OffsetDateTime from = OffsetDateTime.of(2017, 11, 1, 15, 23, 0, 0, ZoneOffset.UTC);
 		OffsetDateTime to = OffsetDateTime.of(2017, 11, 1, 15, 25, 0, 0, ZoneOffset.UTC);
-		Duration duration = Duration.ofMinutes(1);
 
 		DateTimeHelper.historic(duration, from, to)
 				.test()
@@ -65,5 +97,36 @@ public class DateTimeHelperTest {
 						OffsetDateTime.of(2017, 11, 1, 15, 25, 0, 0, ZoneOffset.UTC))
 				.assertComplete();
 	}
+	
+	@Test
+	public void shouldEmitUnlimitedVariableHistoricDateTimes() {
+		Duration minDuration = Duration.ofSeconds(10);
+		Duration maxDuration = Duration.ofMinutes(1);
+		Random random = new Random(1);
+		OffsetDateTime from = OffsetDateTime.of(2017, 11, 1, 15, 23, 0, 0, ZoneOffset.UTC);
 
+		DateTimeHelper.historic(minDuration, maxDuration, random, from)
+				.take(3)
+				.test()
+				.assertValues(OffsetDateTime.of(2017, 11, 1, 15, 23, 0, 0, ZoneOffset.UTC),
+						OffsetDateTime.of(2017, 11, 1, 15, 23, 58, 985000000, ZoneOffset.UTC),
+						OffsetDateTime.of(2017, 11, 1, 15, 24, 23, 573000000, ZoneOffset.UTC))
+				.assertComplete();
+	}
+
+	@Test
+	public void shouldEmitLimitedVariableHistoricDateTimes() {
+		Duration minDuration = Duration.ofSeconds(10);
+		Duration maxDuration = Duration.ofMinutes(1);
+		Random random = new Random(1);
+		OffsetDateTime from = OffsetDateTime.of(2017, 11, 1, 15, 23, 0, 0, ZoneOffset.UTC);
+		OffsetDateTime to = OffsetDateTime.of(2017, 11, 1, 15, 25, 0, 0, ZoneOffset.UTC);
+
+		DateTimeHelper.historic(minDuration, maxDuration, random, from, to)
+				.test()
+				.assertValues(OffsetDateTime.of(2017, 11, 1, 15, 23, 0, 0, ZoneOffset.UTC),
+						OffsetDateTime.of(2017, 11, 1, 15, 23, 58, 985000000, ZoneOffset.UTC),
+						OffsetDateTime.of(2017, 11, 1, 15, 24, 23, 573000000, ZoneOffset.UTC))
+				.assertComplete();
+	}
 }
