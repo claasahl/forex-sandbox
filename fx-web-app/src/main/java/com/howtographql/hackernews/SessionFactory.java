8a91155ddeee7983f513d976431192d834c00d73
Claas Ahlrichs <claasahl@gmail.com>
2018-01-03 16:24:46 +0100

    drafted singleton for accessing SqlSessionFactory

--- fx-web-app/src/main/java/com/howtographql/hackernews/SessionFactory.java
+++ fx-web-app/src/main/java/com/howtographql/hackernews/SessionFactory.java
@@ -1,33 +1,39 @@
 package com.howtographql.hackernews;
 
 import java.io.*;
-import java.util.*;
+import java.util.Properties;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.*;
 import org.flywaydb.core.Flyway;
 
-public class Main {
-	public static void main(String[] args) throws IOException {
+public class SessionFactory {
+	private static SqlSessionFactory factory;
+
+	public static SqlSessionFactory getFactory() {
+		synchronized (factory) {
+			if (factory == null) {
+				try {
+					factory = createFactory();
+				} catch (IOException e) {
+					throw new IllegalStateException(e);
+				}
+			}
+			return factory;
+		}
+	}
+
+	private static SqlSessionFactory createFactory() throws IOException {
 		String settings = "db/settings.properties";
 		Properties props = new Properties();
 		props.load(Resources.getResourceAsStream(settings));
-		
+
 		Flyway flyway = new Flyway();
 		flyway.configure(props);
 		flyway.repair();
 		flyway.migrate();
-		
+
 		String resource = "db/mybatis-config.xml";
 		InputStream inputStream = Resources.getResourceAsStream(resource);
-		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream, props);
-		try(SqlSession session = sqlSessionFactory.openSession()) {
-			LinkMapper mapper = session.getMapper(LinkMapper.class);
-			Link link = new Link("abc", "def");
-			mapper.create(link);
-			List<Link> links = mapper.readAll();
-			System.out.println(links);
-			mapper.delete(link.getId());
-			session.commit();
-		}
+		return new SqlSessionFactoryBuilder().build(inputStream, props);
 	}
 }
