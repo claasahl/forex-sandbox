1ac08d0247eeb79cbfb62ca5421a5a0c31c1932b
Claas Ahlrichs <claasahl@gmail.com>
2018-01-06 10:11:57 +0100

    added DB migration/configuration for user management

---
+++ fx-web-app/src/main/resources/db/migration/V2__create_table_user.sql
@@ -0,0 +1,7 @@
+CREATE TABLE User (
+    ID int NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    PRIMARY KEY (ID)
+); 
