c767307f663a885541a9ea9f50c53996dfd33d28
Claas Ahlrichs <claasahl@gmail.com>
2018-01-08 15:59:24 +0100

    determine current user from HTTP headers

---
+++ fx-web-app/src/main/java/com/howtographql/hackernews/AuthContext.java
@@ -0,0 +1,18 @@
+package com.howtographql.hackernews;
+
+import java.util.Optional;
+import javax.servlet.http.*;
+import graphql.servlet.GraphQLContext;
+
+public class AuthContext extends GraphQLContext {
+	private final User user;
+
+	public AuthContext(User user, Optional<HttpServletRequest> request, Optional<HttpServletResponse> response) {
+		super(request, response);
+		this.user = user;
+	}
+
+	public User getUser() {
+		return user;
+	}
+}
--- fx-web-app/src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
+++ fx-web-app/src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
@@ -1,9 +1,11 @@
 package com.howtographql.hackernews;
 
+import java.util.Optional;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
 import com.coxautodev.graphql.tools.SchemaParser;
 import graphql.schema.GraphQLSchema;
-import graphql.servlet.SimpleGraphQLServlet;
+import graphql.servlet.*;
 
 @WebServlet(urlPatterns = "/graphql")
 public class GraphQLEndpoint extends SimpleGraphQLServlet {
@@ -18,6 +20,17 @@ public class GraphQLEndpoint extends SimpleGraphQLServlet {
 
 	public GraphQLEndpoint() {
 		super(buildSchema());
+	}
+	
+	@Override
+	protected GraphQLContext createContext(Optional<HttpServletRequest> request, Optional<HttpServletResponse> response) {
+	    User user = request
+	        .map(req -> req.getHeader("Authorization"))
+	        .filter(id -> !id.isEmpty())
+	        .map(id -> id.replace("Bearer ", ""))
+	        .map(userRepository::findByToken)
+	        .orElse(null);
+	    return new AuthContext(user, request, response);
 	}
 
 	private static GraphQLSchema buildSchema() {
