c0ec36b046c91ed0fd83ad4b7263b6898ed60ed2
Claas Ahlrichs <claasahl@gmail.com>
2018-01-09 08:02:02 +0100

    removed duplicated SQL

--- fx-web-app/src/main/resources/db/migration/V3__add_token_to_user.sql
+++ fx-web-app/src/main/resources/db/migration/V3__add_token_to_user.sql
@@ -1,9 +1,1 @@
-CREATE TABLE User (
-    ID int NOT NULL AUTO_INCREMENT,
-    name varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
-    password varchar(255) NOT NULL,
-    PRIMARY KEY (ID)
-); 
-
 ALTER TABLE User ADD token varchar(255); 
