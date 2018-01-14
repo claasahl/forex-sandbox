ea3a2cf2887679daa106f3f712933d0024883989
Claas Ahlrichs <claasahl@gmail.com>
2018-01-14 10:53:24 +0100

    dispose onError and onClose

--- fx-web-app/src/main/java/com/howtographql/hackernews/GraphQLWebSocket.java
+++ fx-web-app/src/main/java/com/howtographql/hackernews/GraphQLWebSocket.java
@@ -1,10 +1,14 @@
 package com.howtographql.hackernews;
 
+import java.util.concurrent.atomic.AtomicReference;
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
+import io.reactivex.disposables.Disposable;
 
 @ServerEndpoint("/blub")
 public class GraphQLWebSocket {
+	private final AtomicReference<Disposable> disposable = new AtomicReference<>();
+
 	@OnOpen
 	public void onOpen(Session session) {
 
@@ -17,11 +21,19 @@ public class GraphQLWebSocket {
 
 	@OnError
 	public void onError(Throwable throwable, Session session) {
-
+		dispose();
 	}
 
 	@OnClose
 	public void onClose(Session session) {
-
+		dispose();
+	}
+	
+	private void dispose() {
+		disposable.updateAndGet(d -> {
+			if (d != null)
+				d.dispose();
+			return null;
+		});
 	}
 }
