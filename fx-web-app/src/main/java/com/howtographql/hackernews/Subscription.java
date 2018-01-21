12125a88b142e49b12cb667d39064816bddd619e
Claas Ahlrichs <claasahl@gmail.com>
2018-01-21 09:56:36 +0100

    sample datafetcher for subscriptions

--- fx-web-app/src/main/java/com/howtographql/hackernews/Subscription.java
+++ fx-web-app/src/main/java/com/howtographql/hackernews/Subscription.java
@@ -1,9 +1,22 @@
 package com.howtographql.hackernews;
 
+import java.util.concurrent.TimeUnit;
+import org.reactivestreams.Publisher;
 import graphql.schema.DataFetchingEnvironment;
+import io.reactivex.*;
+import io.reactivex.subjects.*;
 
 public class Subscription {
-	public Link linkCreated(DataFetchingEnvironment env) {
-		return null;
+	private final Subject<Link> links = BehaviorSubject.create();
+
+	public Subscription() {
+		Observable.interval(10, TimeUnit.SECONDS)
+				.map(num -> new Link(num.intValue(), "url#" + num, "desc#" + num, num.intValue()))
+				.doOnNext(links::onNext)
+				.subscribe();
+	}
+
+	public Publisher<Link> linkCreated(DataFetchingEnvironment env) {
+		return links.toFlowable(BackpressureStrategy.BUFFER);
 	}
 }
