806241eb3f8fd7b90173c6019d935d8300e2bf59
Claas Ahlrichs <claasahl@gmail.com>
2018-01-22 06:53:04 +0100

    fixes issue which generated links without any subscription

--- fx-web-app/src/main/java/com/howtographql/hackernews/Subscription.java
+++ fx-web-app/src/main/java/com/howtographql/hackernews/Subscription.java
@@ -1,22 +1,25 @@
 package com.howtographql.hackernews;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 import org.reactivestreams.Publisher;
 import graphql.schema.DataFetchingEnvironment;
 import io.reactivex.*;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.subjects.*;
 
 public class Subscription {
 	private final Subject<Link> links = BehaviorSubject.create();
 
-	public Subscription() {
-		Observable.interval(10, TimeUnit.SECONDS)
-				.map(num -> new Link(num.intValue(), "url#" + num, "desc#" + num, num.intValue()))
-				.doOnNext(links::onNext)
-				.subscribe();
-	}
-
 	public Publisher<Link> linkCreated(DataFetchingEnvironment env) {
-		return links.toFlowable(BackpressureStrategy.BUFFER);
+		Observable<Link> linkGenerator = Observable.interval(1, TimeUnit.SECONDS)
+				.map(num -> new Link(num.intValue(), "url#" + num, "desc#" + num, num.intValue()))
+				.doOnNext(System.out::println)
+				.doOnNext(links::onNext);
+		AtomicReference<Disposable> disposable = new AtomicReference<>();
+		return links
+				.doOnSubscribe(d -> disposable.set(linkGenerator.subscribe()))
+				.doOnDispose(disposable.get()::dispose)
+				.toFlowable(BackpressureStrategy.BUFFER);
 	}
 }
