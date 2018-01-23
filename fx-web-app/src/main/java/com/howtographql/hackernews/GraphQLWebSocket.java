1a71126fa76864054f1d086b1e7570df6ff48c30
Claas Ahlrichs <claasahl@gmail.com>
2018-01-23 06:50:26 +0100

    integrates basic WS-support on the server-side

--- fx-web-app/src/main/java/com/howtographql/hackernews/GraphQLWebSocket.java
+++ fx-web-app/src/main/java/com/howtographql/hackernews/GraphQLWebSocket.java
@@ -1,32 +1,51 @@
 package com.howtographql.hackernews;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
+import io.reactivex.Observable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
-@ServerEndpoint("/blub")
+@ServerEndpoint("/graphql")
 public class GraphQLWebSocket {
 	private final AtomicReference<Disposable> disposable = new AtomicReference<>();
 
 	@OnOpen
 	public void onOpen(Session session) {
-
+		info("Websocket: onOpen");
 	}
 
 	@OnMessage
-	public void onMessage(String message, Session session) {
-
+	public void onMessage(String graphqlQuery, Session session) {
+		info("Websocket: onMessage -- " + graphqlQuery);
+		disposable.set(Observable.interval(3333, TimeUnit.MILLISECONDS, Schedulers.io())
+			.map(l -> "{\"data\":{\"abc\":3333, \"index\":" + l + "}}")
+	        .doOnNext(session.getBasicRemote()::sendText)
+	        .doOnNext(this::info)
+	        .subscribe());
 	}
 
 	@OnError
 	public void onError(Throwable throwable, Session session) {
+		info("Websocket: onError", throwable);
 		dispose();
 	}
 
 	@OnClose
 	public void onClose(Session session) {
+		info("Websocket: onClose");
 		dispose();
+	}
+	
+	private void info(String msg) {
+		System.err.println("############################## " + msg);
+	}
+	
+	private void info(String msg, Throwable t) {
+		info(msg);
+		t.printStackTrace();
 	}
 	
 	private void dispose() {
