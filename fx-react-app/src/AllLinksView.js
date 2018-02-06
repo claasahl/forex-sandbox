50ee8ebe9f8f49d90143bb5a4d38eabf4fa2b8d0
Claas Ahlrichs <claasahl@gmail.com>
2018-02-06 06:52:16 +0100

    added a view that is populated by Apollo using graphql

---
+++ fx-react-app/src/AllLinksView.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const query = gql`
+query AllLinksViewQuery {
+  allLinks {
+    id
+    url
+    description
+    postedBy {
+      id
+      name
+    }
+  }
+}
+`;
+
+class AllLinksView extends React.Component {
+  render() {
+    let {data} = this.props;
+    if(data.loading) {
+      return <div>Loading ...</div>
+    }
+    let {allLinks} = data;
+    return (
+      <div>
+        <ul>
+          {allLinks && allLinks.map(link => (
+            <a key={link.id} href={link.url}>
+              {link.description}
+            </a>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+AllLinksView = graphql(query)(AllLinksView)
+export default AllLinksView;
--- fx-react-app/src/App.js
+++ fx-react-app/src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { ApolloClient } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import AllLinksView from './AllLinksView';
 
 const client = new ApolloClient({
-  // By default, this client will send queries to the
-  //  `/graphql` endpoint on the same host
-  // Pass the configuration option { uri: YOUR_GRAPHQL_API_URL } to the `HttpLink` to connect
-  // to a different host
-  link: new HttpLink(),
+  link: new HttpLink({ uri: 'http://localhost:8080/graphql' }),
   cache: new InMemoryCache()
 });
 
@@ -19,15 +15,7 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
+        <AllLinksView />
       </ApolloProvider>
     );
   }
