f3c5ab38ee27ff8b93571854e7996cd4db3582cf
Claas Ahlrichs <claasahl@gmail.com>
2018-02-07 06:56:53 +0100

    adds simple routing to react application

--- fx-react-app/src/App.js
+++ fx-react-app/src/App.js
@@ -5,6 +5,9 @@ import { ApolloProvider } from 'react-apollo';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import AllLinksView from './AllLinksView';
+import HomeView from './HomeView';
+import CreateLinkView from './CreateLinkView';
+import { BrowserRouter, Route, Link, Router } from 'react-router-dom'
 
 const client = new ApolloClient({
   link: new HttpLink({ uri: 'http://localhost:8080/graphql' }),
@@ -15,7 +18,18 @@ class App extends React.Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <AllLinksView />
+        <BrowserRouter>
+          <div>
+            <nav>
+              <Link to="/">Home</Link>
+              <Link to="/allLinks">All Links</Link>
+              <Link to="/createLink">Create Link</Link>
+            </nav>
+            <Route exact path="/" component={HomeView}/>
+            <Route exact path="/allLinks" component={AllLinksView}/>
+            <Route exact path="/createLink" component={CreateLinkView}/>
+          </div>
+        </BrowserRouter>
       </ApolloProvider>
     );
   }
---
+++ fx-react-app/src/CreateLinkView.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class CreateLinkView extends React.Component {
+    render() {
+        return (
+            <div>create link</div>
+        );
+    }
+}
