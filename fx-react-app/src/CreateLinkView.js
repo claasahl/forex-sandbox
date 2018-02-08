ffa58068aa867d4de663f050471290044886f8d6
Claas Ahlrichs <claasahl@gmail.com>
2018-02-08 06:49:46 +0100

    added a mutation for creating links

--- fx-react-app/src/CreateLinkView.js
+++ fx-react-app/src/CreateLinkView.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
-export default class CreateLinkView extends React.Component {
+const mutation = gql`
+mutation CreateLinkView_createLink($url: String!, $desc: String!) {
+    createLink(url: $url, description: $desc) {
+      id
+    }
+  }
+`;
+
+class CreateLinkView extends React.Component {
+    handleSubmit(e) {
+        e.preventDefault();
+        let formData = new FormData(this.form);
+        this.props
+        .mutate({
+            variables: {
+                url: formData.get('url'),
+                desc: formData.get('description'),
+            },
+        })
+        .then(res => {
+            let {data} = res;
+            window.location.replace('/links/' + data.createLink.id);
+        })
+        .catch(err => console.log(err));
+    }
     render() {
         return (
-            <div>create link</div>
+            <div>
+                <h1>create link</h1>
+                <form
+                    ref={ref => (this.form = ref)}
+                    onSubmit={e => this.handleSubmit(e)}>
+                    <textarea name="url" />
+                    <textarea name="description" />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         );
     }
 }
+
+export default graphql(mutation)(CreateLinkView);
