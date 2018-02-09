11c02cd3625468b9d59fa70dca9b79f586df0c34
Claas Ahlrichs <claasahl@gmail.com>
2018-02-09 06:55:59 +0100

    adds basic support for subscriptions

--- fx-react-app/src/App.js
+++ fx-react-app/src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import './App.css';
 import { ApolloClient } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
 import { HttpLink } from 'apollo-link-http';
+import { getMainDefinition } from 'apollo-utilities';
 import { toIdValue } from 'apollo-utilities';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import AllLinksView from './AllLinksView';
@@ -10,6 +13,29 @@ import HomeView from './HomeView';
 import CreateLinkView from './CreateLinkView';
 import DetailLinkView from './DetailLinkView';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:8080/graphql'
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:8080/graphql`,
+  options: {
+    reconnect: true
+  }
+});
+
+// using the ability to split links, you can send data to each link
+// depending on what kind of operation is being sent
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  httpLink,
+);
 
 const cache = new InMemoryCache({
   cacheResolvers: {
@@ -19,7 +45,7 @@ const cache = new InMemoryCache({
   }
 });
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'http://localhost:8080/graphql' }),
+  link,
   cache
 });
 
