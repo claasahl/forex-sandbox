1786d062bb53c52468ac1250c1f1c7c1f38c9be3
Claas Ahlrichs <claasahl@gmail.com>
2018-02-23 06:54:02 +0100

    adds the basic setup for the apollo graphql client

--- fx-web-frontend/src/main/frontend/src/App.js
+++ fx-web-frontend/src/main/frontend/src/App.js
@@ -1,31 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
-import HomeView from "./views/HomeView";
-import BrokersView from "./views/BrokersView";
-import ApiExplorer from "./views/ApiExplorer";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import Routing from "./views/Routing";
 import "./App.css";
+
+const link = new HttpLink({
+  uri: "http://localhost:8080/fx/graphql",
+});
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+  link,
+  cache,
+});
 
 class App extends React.Component {
   render() {
     return (
-      <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/brokers">Brokers</Link>
-            </li>
-            <li>
-              <Link to="/api/explorer">API Explorer</Link>
-            </li>
-          </ul>
-          <Route exact path="/" component={HomeView} />
-          <Route exact path="/brokers" component={BrokersView} />
-          <Route exact path="/api/explorer" component={ApiExplorer} />
-        </div>
-      </Router>
+      <ApolloProvider client={client}>
+        <Routing />
+      </ApolloProvider>
     );
   }
 }
---
+++ fx-web-frontend/src/main/frontend/src/views/Routing.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import HomeView from "./HomeView";
+import BrokersView from "./BrokersView";
+import ApiExplorer from "./ApiExplorer";
+
+export default class Routing extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/brokers">Brokers</Link>
+            </li>
+            <li>
+              <Link to="/api/explorer">API Explorer</Link>
+            </li>
+          </ul>
+          <Route exact path="/" component={HomeView} />
+          <Route exact path="/brokers" component={BrokersView} />
+          <Route exact path="/api/explorer" component={ApiExplorer} />
+        </div>
+      </Router>
+    );
+  }
+}
