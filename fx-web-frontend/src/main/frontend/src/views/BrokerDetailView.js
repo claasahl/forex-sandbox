ee30ab0cae93140b8302b41bad59b52ed0216d43
Claas Ahlrichs <claasahl@gmail.com>
2018-03-02 06:57:28 +0100

    added details to broker view

--- fx-web-frontend/src/main/frontend/src/views/BrokerDetailView.js
+++ fx-web-frontend/src/main/frontend/src/views/BrokerDetailView.js
@@ -10,7 +10,21 @@ class BrokerDetailView extends React.Component {
     }
     const { brokers } = data;
     const broker = brokers[0] || {};
-    return <div>{broker.name}</div>;
+    return (
+      <div>
+        <h1>{broker.name}</h1>
+        <ul>
+          {broker.symbols.map(symbol => {
+            return (
+              <li key={symbol.id}>
+                {symbol.name}
+                {symbol.duration && ` - ${symbol.duration}`}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
   }
 }
 
