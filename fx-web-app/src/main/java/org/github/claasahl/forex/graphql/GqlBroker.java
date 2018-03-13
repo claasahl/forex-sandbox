20b9bccb7bcd60effbd8d573df8beb8850c4236e
Claas Ahlrichs <claasahl@gmail.com>
2018-03-13 06:47:18 +0100

    added documentation to a few Gql*-classes

--- fx-web-app/src/main/java/org/github/claasahl/forex/graphql/GqlBroker.java
+++ fx-web-app/src/main/java/org/github/claasahl/forex/graphql/GqlBroker.java
@@ -2,6 +2,15 @@ package org.github.claasahl.forex.graphql;
 
 import org.github.claasahl.forex.database.BrokerInstance;
 
+/**
+ * The class {@link GqlBroker} is exposed through GraphQL. It wraps internal
+ * objects and (re-)shapes them as described in the GraphQL schema. This class
+ * also adds an extra layer of abstraction which makes it more difficult to leak
+ * (i.e. unintentionally expose) details about the wrapped internal objects.
+ * 
+ * @author Claas Ahlrichs
+ *
+ */
 class GqlBroker {
 	private final BrokerInstance brokerInstance;
 
@@ -9,14 +18,22 @@ class GqlBroker {
 		this.brokerInstance = brokerInstance;
 	}
 
-	protected BrokerInstance getBrokerInstance() {
-		return brokerInstance;
-	}
-
+	/**
+	 * Returns this broker's id.
+	 * 
+	 * @see BrokerInstance#getId()
+	 * @return this broker's id
+	 */
 	protected String getId() {
 		return brokerInstance.getId();
 	}
 
+	/**
+	 * Returns this broker's name.
+	 * 
+	 * @see BrokerInstance#getName()
+	 * @return this broker's name
+	 */
 	protected String getName() {
 		return brokerInstance.getName();
 	}
--- fx-web-app/src/main/java/org/github/claasahl/forex/graphql/GqlRate.java
+++ fx-web-app/src/main/java/org/github/claasahl/forex/graphql/GqlRate.java
@@ -1,8 +1,17 @@
 package org.github.claasahl.forex.graphql;
 
 import java.time.OffsetDateTime;
-import org.github.claasahl.forex.broker.Rate;
+import org.github.claasahl.forex.broker.*;
 
+/**
+ * The class {@link GqlRate} is exposed through GraphQL. It wraps internal
+ * objects and (re-)shapes them as described in the GraphQL schema. This class
+ * also adds an extra layer of abstraction which makes it more difficult to leak
+ * (i.e. unintentionally expose) details about the wrapped internal objects.
+ * 
+ * @author Claas Ahlrichs
+ *
+ */
 class GqlRate {
 	private final GqlRateFilter filter;
 	private final Rate rate;
@@ -12,34 +21,61 @@ class GqlRate {
 		this.rate = rate;
 	}
 
-	protected GqlRateFilter getFilter() {
-		return filter;
-	}
-
-	protected Rate getRate() {
-		return rate;
-	}
-
+	/**
+	 * Returns this rate's broker (ID).
+	 * 
+	 * @return this rate's broker (ID)
+	 */
 	protected String getBrokerId() {
 		return filter.getBrokerId();
 	}
 
+	/**
+	 * Returns this rate's symbol.
+	 * 
+	 * @see RateFilter#getSymbol()
+	 * @return this rate's symbol
+	 */
 	protected String getSymbol() {
 		return filter.getFilter().getSymbol();
 	}
 
+	/**
+	 * Returns this rate's date and time.
+	 * 
+	 * @see Rate#getDateTime()
+	 * @return this rate's date and time
+	 */
 	protected OffsetDateTime getDateTime() {
 		return rate.getDateTime();
 	}
 
+	/**
+	 * Returns this rate's bid price.
+	 * 
+	 * @see Rate#getBid()
+	 * @return this rate's bid price
+	 */
 	protected double getBid() {
 		return rate.getBid();
 	}
 
+	/**
+	 * Returns this rate's ask price.
+	 * 
+	 * @see Rate#getAsk()
+	 * @return this rate's ask price
+	 */
 	protected double getAsk() {
 		return rate.getAsk();
 	}
-	
+
+	/**
+	 * Returns this rate's spread.
+	 * 
+	 * @see Rate#getSpread()
+	 * @return this rate's spread
+	 */
 	protected double getSpread() {
 		return rate.getSpread();
 	}
